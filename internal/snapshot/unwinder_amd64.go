2bc2faf60e2ff9780abe7bdc9bd4d4c65d125eb0
Copybara <copybara@example.com>
2024-07-01 21:34:50 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 0dd36174c31b3881bee118b19d4cf1859e924e03

--- internal/snapshot/unwinder_amd64.go
+++ internal/snapshot/unwinder_amd64.go
@@ -2,7 +2,14 @@
 
 package snapshot
 
-func adjustCFA(fps []uintptr, stackTopSp uintptr) []uintptr {
+// adjustCFA adjust the base pointer value to below the return address.
+//
+// See https://github.com/golang/go/blob/94982a07/src/cmd/compile/abi-internal.md?plain=1#L448-L473
+func adjustCFA(
+	fps []uintptr,
+	// The stack top value is not used on amd64.
+	_ uintptr,
+) []uintptr {
 	for i := range fps {
 		fps[i] += 16
 	}
--- internal/snapshot/unwinder_arm64.go
+++ internal/snapshot/unwinder_arm64.go
@@ -2,6 +2,11 @@
 
 package snapshot
 
+// adjustCFA adjust the base pointer value to point to the frame base for the current
+// frame. On arm64, this is actually the next frame's frame pointer. The root frame's
+// CFA is the stack top.
+//
+// See https://github.com/golang/go/blob/94982a07/src/cmd/compile/abi-internal.md?plain=1#L568-L601
 func adjustCFA(fps []uintptr, stackTopSp uintptr) []uintptr {
 	for i := 0; i < len(fps)-1; i++ {
 		fps[i] = fps[i+1] + 8
